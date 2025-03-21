5975a2bc2e9cda1ab4e619e0fbe84fd3824720e7
Vitor Andrade <80630423+Vitor-A-Monteiro@users.noreply.github.com>
2025-03-21 10:30:55 -0300

    Update ibgeService.js

--- src/services/ibgeService.js
+++ src/services/ibgeService.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://servicodados.ibge.gov.br/api/v3/agregados/73/periodos/-60/variaveis/9286,9287?localidades=1';
-const MAX_RETRIES = 3; // Defina o número máximo de tentativas
-
+const MAX_RETRIES = 3; 
 const fetchData = async (retries = 0) => {
   try {
     const response = await axios.get(API_URL);
@@ -18,8 +17,8 @@ const fetchData = async (retries = 0) => {
 
     if (retries < MAX_RETRIES) {
       console.log(`Tentando novamente em 1 segundo...`);
-      await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 1 segundo
-      return fetchData(retries + 1); // Chama a função fetchData novamente
+      await new Promise(resolve => setTimeout(resolve, 1000)); 
+      return fetchData(retries + 1); 
     } else {
       console.error("Número máximo de tentativas atingido. Desistindo.");
       if (error.response && error.response.status === 500) {
