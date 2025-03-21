e5e362b4f26164911bbf55ca9d776d937764d024
Vitor Andrade <80630423+Vitor-A-Monteiro@users.noreply.github.com>
2025-03-21 10:31:48 -0300

    Update ibgeService.test.js

--- src/services/ibgeService.test.js
+++ src/services/ibgeService.test.js
@@ -1,7 +1,7 @@
 import fetchData from './ibgeService';
 import axios from 'axios';
 
-jest.mock('axios'); // Mocks the axios module
+jest.mock('axios'); 
 
 describe('ibgeService', () => {
   it('should fetch data successfully from the API', async () => {
@@ -14,21 +14,21 @@ describe('ibgeService', () => {
       }]
     }];
 
-    axios.get.mockResolvedValue({ data: mockData }); // Mocks the axios.get method
+    axios.get.mockResolvedValue({ data: mockData }); 
 
     const data = await fetchData();
 
-    expect(axios.get).toHaveBeenCalledTimes(1); // Verifies that axios.get was called once
+    expect(axios.get).toHaveBeenCalledTimes(1); 
     expect(data).toEqual([{
       ano: '2020',
       pibTotal: 1000,
       pibPerCapita: 1000
-    }]); // Verifies that the function returns the expected data
+    }]); 
   });
 
   it('should handle errors when fetching data from the API', async () => {
-    axios.get.mockRejectedValue(new Error('API Error')); // Mocks the axios.get method to reject with an error
+    axios.get.mockRejectedValue(new Error('API Error')); 
 
-    await expect(fetchData()).rejects.toThrow('API Error'); // Verifies that the function throws an error
+    await expect(fetchData()).rejects.toThrow('API Error'); 
   });
 });
